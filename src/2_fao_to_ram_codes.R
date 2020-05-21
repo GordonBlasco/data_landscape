d66dbe6d4d86eb74d1577c35e4f3e47ea26a0f74
Gordon Blasco <gordonblasco@gmail.com>
2020-05-20 17:18:25 -0700

    start the ram name pull

--- src/2_fao_to_ram_codes.R
+++ src/2_fao_to_ram_codes.R
@@ -11,13 +11,60 @@ library(tidyverse)
 library(ramlegacy)
 library(rfishbase)
 
-## Load data
+## Load data from FAO and RAM
 ##################################################
 
 source("src/file_names.R") # Sets file directories 
 fao_production <- read_csv(file.path(dir_raw_data, "/FAO/production/TS_FI_PRODUCTION.csv")) # Raw fao data
 nei_levels <- read_csv("data/nei_codes.csv")
 
-fao_species <- read_csv(file.path(dir_raw_data, "/FAO/production/CL_FI_SPECIES_GROUPS.csv")) # Species ref
-val_fishbase <- read_csv(file.path(dir_data, "fishbase_name_val.csv"))
+fao_species <- read_csv(file.path(dir_raw_data, "/FAO/production/CL_FI_SPECIES_GROUPS.csv")) %>% # Species ref
+  rename(SPECIES = `3Alpha_Code`) %>% 
+  filter(SPECIES %in% nei_levels$SPECIES) %>% 
+  left_join(., nei_levels)
+
+
+
+
+RAM_all <- load_ramlegacy()
+RAM_meta <- RAM_all[["metadata"]]
+
+
+
+## Join RAM and FAO
+##################################################
+
+### first join on scientific name
+ram_names <- RAM_meta %>% 
+  distinct(scientificname) %>% 
+  left_join(., fao_species, by = c("scientificname" = "Scientific_Name")) 
+
+
+
+ram_temp1 <- ram_names %>% 
+  filter(is.na(SPECIES)) %>% 
+  select(scientificname)
+
+
+
+
+
+
+
+
+
+fao_to_ram <- ram_names %>% # must be 360 obs long
+  filter(!is.na(SPECIES)) # at 284, 76 more to go.
+
+
+
+
+
+
+
+
+
+
+
+
 
