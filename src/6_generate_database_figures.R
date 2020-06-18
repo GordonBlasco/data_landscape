a75a895a951ae72bdd28776749aa83605546b2fc
Gordon Blasco <gordonblasco@gmail.com>
2020-06-18 12:32:10 -0700

    fixed up database figures

--- src/6_generate_database_figures.R
+++ src/6_generate_database_figures.R
@@ -10,6 +10,10 @@
 ################################################################################
 library(tidyverse)
 library(patchwork)
+library(janitor)
+library(RColorBrewer)
+library(gridExtra)
+library(ggpubr)
 
 
 source("~/github/aquaculture/src/directories.R") # Sets file directories 
@@ -74,21 +78,200 @@ tile_df <- spp_info %>%
                names_to = "database", 
                values_to = "status") %>% 
   filter(!is.na(status)) %>% 
-  arrange(Major_Group) %>% 
+  mutate(Major_Group2 = factor(Major_Group, levels = c("NEI",
+                                                       "AMPHIBIA, REPTILIA", 
+                                                       "CRUSTACEA", 
+                                                       "INVERTEBRATA AQUATICA",
+                                                       "MAMMALIA", 
+                                                       "MOLLUSCA",                 
+                                                       "PLANTAE AQUATICAE",
+                                                       "PISCES"))) %>% 
+  arrange(Major_Group2) %>% 
   mutate(SPECIES = fct_inorder(SPECIES))
 
 
-ggplot(tile_df, aes(x = database, y = SPECIES, fill = Major_Group))+
+pal <- c("#D53E4F", "#FC8D59", "#FEE08B", "#FFFFBF", "#E6F598", "#99D594", "#3288BD")
+pal2 <- c("grey", pal)
+
+
+db_tiles <- ggplot(tile_df, aes(x = database, y = SPECIES, fill = Major_Group2))+
   geom_tile() +
-  theme(axis.text.y = element_blank())
+  labs_pubr() +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())+
+  scale_fill_manual(values = pal)+
+  theme(legend.position = "none")+
+  labs(
+    title = "A) Species presence in datasets",
+    x = "",
+    y = ""
+   ) + 
+   theme(
+   #plot.title.position = "plot"
+   )+
+  scale_x_discrete(expand = c(0,0))+
+  theme(
+    plot.title = element_text(size = 20, colour = "black", lineheight = 1, face = "bold"), 
+  )
+  
+
+
+db_tiles
 
 
 
+## Database Tonnage Comparison
+################################################################################
+fao_core <- fao_production %>% 
+  left_join(spp_info)
+
+fao_tonnage <- fao_core %>% 
+  filter(FAO == "yes") %>% 
+  mutate(Major_fixed = case_when(
+    id_level == "Nei" ~ "NEI", 
+    id_level == "Species" ~ Major_Group)) %>% 
+  group_by(Major_fixed) %>% 
+  summarise(tonnage = sum(QUANTITY, na.rm = TRUE)) %>% 
+  rename(Major_Group = Major_fixed) %>% 
+  mutate(database = "FAO")
+
+iucn_tonnage <- fao_core %>% 
+  filter(IUCN == "yes") %>% 
+  group_by(Major_Group) %>% 
+  summarise(tonnage = sum(QUANTITY, na.rm = TRUE))%>% 
+  mutate(database = "IUCN")
+
+ram_tonnage <- fao_core %>% 
+  filter(RAM == "yes") %>% 
+  group_by(Major_Group) %>% 
+  summarise(tonnage = sum(QUANTITY, na.rm = TRUE))%>% 
+  mutate(database = "RAM")
+
+all_tonnage <- fao_core %>% 
+  filter(RAM == "yes" &
+         IUCN == "yes") %>% 
+  group_by(Major_Group) %>% 
+  summarise(tonnage = sum(QUANTITY, na.rm = TRUE))%>% 
+  mutate(database = "In All")
 
 
-## Create biomass species comparison
+blank_info <- tribble(
+  ~database, ~Major_Group, ~tonnage, 
+  "blank", "PISCES", 0, 
+  "space", "PISCES", 0
+)
+
+
+tonnage_df <- fao_tonnage %>% 
+  bind_rows(iucn_tonnage,
+            ram_tonnage,
+            all_tonnage, 
+            blank_info) %>% 
+  mutate(Major_Group = factor(Major_Group, levels = c("NEI" ,
+                                                      "AMPHIBIA, REPTILIA", 
+                                                      "CRUSTACEA", 
+                                                      "INVERTEBRATA AQUATICA",
+                                                      "MAMMALIA", 
+                                                      "MOLLUSCA",                 
+                                                      "PLANTAE AQUATICAE",
+                                                      "PISCES"))) %>% 
+  mutate(database = factor(database, levels = c("space",
+                                                "blank", 
+                                                "In All", 
+                                                "RAM", 
+                                                "IUCN", 
+                                                "FAO"))) %>% 
+  mutate(text = case_when(
+    database == "FAO" & Major_Group == "PISCES" ~ "FAO ",
+    database == "RAM" & Major_Group == "PISCES"~ "RAM ",
+    database == "IUCN"& Major_Group == "PISCES" ~ "IUCN ",
+    database == "In All"& Major_Group == "PISCES" ~ "All ",
+    database == "blank"& Major_Group == "PISCES" ~ "",
+    database == "space"& Major_Group == "PISCES" ~ ""
+  )) 
+  
+  
+
+
+
+all_biomass_limit <- sum(fao_core$QUANTITY)
+
+geom.text.size = 14
+theme.size = (14/5) * geom.text.size
+
+
+db_ton <- ggplot(tonnage_df, aes(x = database, y = tonnage, fill = Major_Group))+
+  geom_bar(width = 0.9, stat="identity", color = "black") +
+  coord_polar(theta = "y")+
+  scale_y_continuous(limits = c(0,(all_biomass_limit*1.05))) +
+  scale_fill_manual(values = pal2)+
+ # scale_fill_brewer(palette = "Spectral") +
+  #geom_text(data = biomass_plot_df, hjust = 1.02, size = 3.5, aes(x = database, y = 0, label = text)) +
+  ggtitle("B) Tonnage accounted by each database") +
+  theme_minimal() +
+  theme(#legend.position = "none",
+        panel.grid = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank()) +
+  #geom_text(aes(y = total_bio + 0.5,label = total_bio)) + # add value
+  geom_text(aes(y = (0),
+                hjust = "right",
+                label = text,
+                fontface = "bold"),
+            size = 5)+
+  theme(text = element_text(face = "plain", 
+                            colour = "black", 
+                            size = 14, 
+                            #lineheight = 0.9, 
+                            #hjust = 0.5, 
+                            #vjust = 0.5, 
+                            #angle = 0, 
+                            #margin = margin(), 
+                            debug = FALSE), 
+              #axis.text.x = element_text(size = rel(0.86), colour = "black", face = "bold"), 
+              #axis.text.y = element_text(size = rel(0.86), colour = "black", face = "bold"), 
+              #axis.title = element_text(size = rel(1), colour = "black", face = "bold"), 
+              plot.title = element_text(size = 20, colour = "black", lineheight = 1, face = "bold"), 
+              legend.title = element_text(size = 14, 
+                                          face = "bold", 
+                                          colour = "black"), 
+              legend.text = element_text(size = rel(0.7), face = "plain", colour = "black"))+
+  theme(plot.title = element_text(hjust = 0.5))
+
+  
+
+db_ton
+
+
+
+patch1 <- db_tiles+db_ton+ plot_layout(guides = 'collect')
+  
+
+
+## Create biomass species comparison - maybe delete -
 ################################################################################
 fao_core <- fao_production 
+
+all <- spp_info %>% 
+  filter(FAO == "yes"&
+           RAM == "yes"&
+           IUCN == "yes") %>% 
+  group_by(ISSCAAP_Group) %>% 
+  summarise(total = n())
+
+
+neither <- spp_info %>% 
+  filter(FAO == "yes"&
+           is.na(RAM)&
+           is.na(IUCN)) %>%  
+  select(Name_En, Scientific_Name, SPECIES)
+
+
+ram_spp_list <- spp_info %>% 
+  filter(RAM == "yes") %>%  
+  select(Name_En, Scientific_Name, SPECIES)
+
+
 
 prod_all <- fao_core %>% 
   filter(YEAR == 2016) %>% 
@@ -125,13 +308,17 @@ prod_none <- fao_core %>%
   left_join(prod_all) %>% 
   pivot_longer(cols=c("all_bio", "none_bio"))
 
+
+prod_ram <- fao_core %>% 
+  filter()
+
 ggplot(prod_none, aes(x = Major_Group, y = value, fill = name))+
   geom_col(position = "dodge")
 
 
 
 
-prod_none <- fao_core %>% 
+prod_none1 <- fao_core %>% 
   filter(SPECIES %in% neither$SPECIES) %>% 
   filter(YEAR == 2016) %>% 
   filter(SOURCE == 4) %>% 
@@ -148,7 +335,7 @@ prod_none <- fao_core %>%
   top_n(1, wt = biomass)
 
 
-ggplot(prod_none, aes(x = Major_Group, y = biomass, fill = SPECIES))+
+ggplot(prod_none1, aes(x = Major_Group, y = biomass, fill = SPECIES))+
   geom_col(position="stack", stat="identity")+
   theme(legend.position = "none")
 
@@ -178,46 +365,86 @@ total_spp_listed <- spp_info %>%
     specific_species = n()
   )
 
+total_nei_spp_items <- spp_info %>% 
+  filter(id_level == "Nei") %>% 
+  group_by(Major_Group) %>% 
+  summarise(
+    nei_species_items = n()
+  )
+
+
 total_spp_ram <- spp_info %>% 
-  filter(!is.na(ram)) %>% 
+  filter(!is.na(RAM)) %>% 
   group_by(Major_Group) %>% 
   summarise(
     in_ram = n()
   )
 
 total_spp_iucn <- spp_info %>% 
-  filter(!is.na(iucn)) %>% 
+  filter(!is.na(IUCN)) %>% 
   group_by(Major_Group) %>% 
   summarise(
     in_iucn = n()
   )
 
+total_spp_in_all <- spp_info %>% 
+  filter(!is.na(RAM)&
+        !is.na(IUCN)) %>% 
+  group_by(Major_Group) %>% 
+  summarise(
+    in_all = n()
+  )
+
+
 
 final_table <- total_spp %>% 
+  left_join(total_nei_spp_items) %>% 
   left_join(total_spp_listed) %>% 
   left_join(total_spp_ram) %>% 
   left_join(total_spp_iucn) %>% 
-  adorn_totals()
+  left_join(total_spp_in_all) %>% 
+  mutate_if(is.integer, replace_na, 0) %>% 
+  adorn_totals() %>% 
+  arrange(total_spp) %>% 
+  rename(
+    "Total Species"="total_spp",
+    "NEI Species Items"="nei_species_items",
+    "Specific Species"="specific_species",
+    "Covered in RAM"="in_ram",
+    "Covered in IUCN"="in_iucn",
+    "Covered in Both"="in_all"
+  )
 
+final_table
+
+layout <- c(
+  patchwork::area(t = 1, l = 1, b = 6, r = 5), 
+  patchwork::area(t = 1, l = 5, b = 6, r = 11),
+  patchwork::area(t = 7, l = 3, b = 9, r = 10)
+)
+plot(layout)
+
+layout2 <- c(
+  patchwork::area(t = 1, l = 1, b = 9, r = 4), 
+  patchwork::area(t = 1, l = 5, b = 6, r = 11),
+  patchwork::area(t = 7, l = 5, b = 9, r = 11)
+)
+
+plot(layout2)
+
+
+
+
+
+db_tiles + db_ton + gridExtra::tableGrob(final_table, rows = NULL, theme=Tmin) + 
+  plot_layout(design = layout)
 
 
 ##  supplement tables
 ################################################################################
 
 
-all <- spp_info %>% 
-  filter(FAO == "yes"&
-           RAM == "yes"&
-           IUCN == "yes") %>% 
-  group_by(ISSCAAP_Group) %>% 
-  summarise(total = n())
 
-
-neither <- spp_info %>% 
-  filter(FAO == "yes"&
-           is.na(RAM)&
-           is.na(IUCN)) %>%  
-  select(Name_En, Scientific_Name, SPECIES)
 
 
 
